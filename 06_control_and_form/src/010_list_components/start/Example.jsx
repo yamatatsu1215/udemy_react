cdc9f699f0a51f3c9e271c31a11556bd272dfaad
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-06-06 13:44:11 +0900

    add commti

--- 06_control_and_form/src/010_list_components/start/Example.jsx
+++ 06_control_and_form/src/010_list_components/start/Example.jsx
@@ -4,15 +4,19 @@ const animals = ["Dog", "Cat", "Rat"];
 const Example = () => {
   const animallist = [];
   for(const animal of animals) {
-
+    animallist.push(<li>{animal}</li>);
   }
+
+  const helloAnimals = animals.map((animal) => <li>Hello, {animal}</li>)
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        <li>{animals[0]}</li>
+        {/* <li>{animals[0]}</li>
         <li>{animals[1]}</li>
-        <li>{animals[2]}</li>
+        <li>{animals[2]}</li> */}
+        {animallist}
+        {helloAnimals}
       </ul>
     </>
   );
