87949b7802b7fe8dea7b7a0788b5b6dcbfd4d102
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-08-06 21:28:06 +0900

    fix chakra-ui second

--- 07_styling_component/src/050_chakra_ui/start/components/List.jsx
+++ 07_styling_component/src/050_chakra_ui/start/components/List.jsx
@@ -1,4 +1,5 @@
-import { VStack, StackDivider } from "@chakra-ui/react";
+import { VStack, StackDivider, HStack } from "@chakra-ui/react";
+import { VscCheck } from 'react-icons';
 
 const List = ({todos, deleteTodo}) => {
     const complete = (id) => {
@@ -7,13 +8,18 @@ const List = ({todos, deleteTodo}) => {
     return (
         <VStack
             divider={<StackDivider borderColor='gray.200'></StackDivider>}
+            borderColor="black.100"
+            borderWidth="1px"
+            borderRadius="3px"
+            p={5}
+            alignItems="start"
         >
             {todos.map(todo => {
                 return (
-                    <div key={todo.id}>
+                    <HStack key={todo.id}>
                         <button onClick={() => complete(todo.id)}>完了</button>
                         <span>{todo.content}</span>
-                    </div>
+                    </HStack>
                 )
             })}
         </VStack>
