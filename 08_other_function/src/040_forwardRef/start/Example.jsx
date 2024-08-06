5400efba56be270a216a9536634e6b7db5b52126
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-08-06 21:57:08 +0900

    fix userRef second

--- 08_other_function/src/040_forwardRef/start/Example.jsx
+++ 08_other_function/src/040_forwardRef/start/Example.jsx
@@ -1,10 +1,13 @@
 import { useRef, forwardRef } from "react";
 
+const Input = ({ ref }) => {
+  return <input type="text" ref={ref}></input>
+}
 const Example = () => {
   const ref = useRef();
   return (
     <>
-      <input type="text" ref={ref} />
+      <Input customRef={ref} />
       <button onClick={() => ref.current.focus()}>
         インプット要素をフォーカスする
       </button>
