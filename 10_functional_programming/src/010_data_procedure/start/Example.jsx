943e7335582499989d6e0abbd4f13fcff33a5005
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-08-06 22:22:21 +0900

    fix nums component

--- 10_functional_programming/src/010_data_procedure/start/Example.jsx
+++ 10_functional_programming/src/010_data_procedure/start/Example.jsx
@@ -1,4 +1,26 @@
 const Example = () => {
+
+  const nums = [1,2,3];
+  const sum = (arry) => {
+    const nums = arry;
+    let sumValue = 0;
+    for(let i = 0; i < nums.length; i++) {
+      sumValue += nums[i];
+    }
+    return sumValue;
+  } 
+
+
+  const numobj = {
+    nums: [1,2,3],
+    sum() {
+      const nums = this.nums;
+      let sumValue = 0;
+      for(let i = 0; i < nums.length; i++) {
+        sumValue += nums[i];
+      }
+    }
+  }
   return (
     <>
       <h3>状態管理と処理を分離</h3>
