139c07546635f078ec0ced33cbcc70d87b88deb4
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-08-06 21:47:24 +0900

    fix useRef

--- 08_other_function/src/030_useRef/start/Example.jsx
+++ 08_other_function/src/030_useRef/start/Example.jsx
@@ -1,23 +1,41 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Case1 = () => {
   const [value, setValue] = useState("");
+  const inputref = useRef()
+
 
   return (
     <div>
       <h3>ユースケース1</h3>
-      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick>
+      <input type="text" ref={inputref} value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={() => inputref.current.focus()}>
         インプット要素をフォーカスする
       </button>
     </div>
   );
 };
 
+const Case2 = () => {
+  return (
+    <div>
+      <video>
+        <source src="sample.mp4"></source>
+      </video>
+      <button onClick={() => {
+        setPlaying(prev => !prev);
+      }}>
+        { playing ? 'Stop' : 'Play' }
+      </button>
+    </div>
+  )
+}
+
 const Example = () => {
   return (
     <>
       <Case1 />
+      <Case2></Case2>
     </>
   );
 };
