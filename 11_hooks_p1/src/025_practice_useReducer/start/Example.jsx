32f8bc6c4a0155dbbdcec9845a80a2c6e2ebd4d5
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-08-07 15:55:09 +0900

    fix useReducer practice

--- 11_hooks_p1/src/025_practice_useReducer/start/Example.jsx
+++ 11_hooks_p1/src/025_practice_useReducer/start/Example.jsx
@@ -2,7 +2,20 @@ import { useReducer } from "react";
 
 const CALC_OPTIONS = ["add", "minus", "divide", "multiply"];
 
-const reducer = () => {}
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case "add":
+      return { ...state, result: state.a + state.b };
+    case "minus":
+      return { ...state, result: state.a - state.b };
+    case "multiply":
+      return { ...state, result: state.a * state.b };
+    case "divide":
+      return { ...state, result: state.a / state.b };
+      default:
+        throw new Error('不明なタイプです');
+  }
+};
 
 const Example = () => {
   const initState = {
@@ -13,19 +26,17 @@ const Example = () => {
 
   const [state, dispatch] = useReducer(reducer, initState);
 
-  const calculate = (e) => {
-    
-  };
+  const calculate = (e) => {};
 
   const numChangeHandler = (e) => {
-    
-  }
+    dispatch({ type: 'change', payload: { name: e.target.name }})
+  };
 
   return (
     <>
-    <h3>練習問題</h3>
-    <p>useReducerを使って完成コードと同じ機能を作成してください。</p>
-      {/* <div>
+      <h3>練習問題</h3>
+      <p>useReducerを使って完成コードと同じ機能を作成してください。</p>
+      <div>
         a:
         <input
           type="number"
@@ -43,9 +54,8 @@ const Example = () => {
           onChange={numChangeHandler}
         />
       </div>
-      <select value={state.type} onChange={calculate}>
-      </select>
-      <h1>結果：{state.result}</h1> */}
+      <select value={state.type} onChange={calculate}></select>
+      <h1>結果：{state.result}</h1>
     </>
   );
 };
