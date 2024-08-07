8e3e6afc5058424ca98d39a453ebfa86153dab83
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-08-07 15:43:12 +0900

    fix useReducer

--- 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
+++ 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
@@ -1,7 +1,26 @@
+import { useReducer, useState } from "react";
+
 const Example = () => {
-  
+  const [state, setState] = useState(0);
+  const [rsate, dispatch] = useReducer((prev) => ++prev,0);
+
+  const countup = () => {
+    setState((prev) => ++prev);
+  };
+
+  const rcountup = () => {
+    dispatch();
+  }
   return (
     <>
+      <div>
+        <h3>{state}</h3>
+        <button onClick={countup}>+</button>
+      </div>
+      <div>
+        <h3>{rstate}</h3>
+        <button onClick={rcountup}>+</button>
+      </div>
     </>
   );
 };
