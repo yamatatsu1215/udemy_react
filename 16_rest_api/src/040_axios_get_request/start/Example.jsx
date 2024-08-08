7cbe9e6131f862692b5dd6543ad2bfa404504e88
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-08-08 22:48:51 +0900

    fix axios

--- 16_rest_api/src/040_axios_get_request/start/Example.jsx
+++ 16_rest_api/src/040_axios_get_request/start/Example.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+import axios from 'axios';
 const Example = () => {
+  useEffect(async() => {
+    const res = await axios.get('http://localhost:3003/user')
+      console.log(res.data)
+    });
+  
   return <></>;
 };
 
