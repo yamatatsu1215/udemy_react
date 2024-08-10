2928606ec4b5aa4b1127ba84db144f4d662e93d7
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-08-10 21:53:53 +0900

    fix test first

---
+++ 19_test/src/010_first_test_run/start/components/Greet.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Greet from "./Greet"
+
+test('h1が存在するかどうか', () => { 
+    render(<Greet></Greet>);
+    const h1EL = screen.getByText("こんにちは");
+    expect(h1EL).toBeInTheDocument();
+})
