d25e00a32d640018a70f63a4f412b909f9fa1ef1
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-08-10 22:19:15 +0900

    fix counter test

--- 19_test/src/030_how_to_test/start/components/Counter.test.jsx
+++ 19_test/src/030_how_to_test/start/components/Counter.test.jsx
@@ -3,4 +3,15 @@ Arrage:(テストデータ、条件、環境の設定).
 Act:(ロジックの実行、関数の実行).
 Assertion:(実行結果と期待する結果の比較).
 */
-test('ダミーテスト:削除して記載してください。', () => {});
+import { fireEvent, render , screen} from "@testing-library/react";
+import Counter from "./Counter";
+
+test('｢カウントアップ｣押下で｢現在のカウント｣が+1されるか', () => {
+    render(<Counter originCount={0}></Counter>);
+
+    const btn = screen.getByRole('button', { name: 'カウントアップ' })
+    fireEvent.click(btn);
+
+    const spanEl = screen.getByText('現在のカウント:1')
+    expect(spanEl).toBeInTheDocument();
+})
