18b251a42b2b8e0aef17fe733bc2e46435014d1d
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-08-11 21:29:54 +0900

    fix typescript generic

--- 20_typescript/src/090_generics/start/Example.ts
+++ 20_typescript/src/090_generics/start/Example.ts
@@ -1,5 +1,20 @@
 const Example = () => {
   // 練習用
+  const repeatstr = (value: string, times: number): string[] => {
+    return new Array(times).fill(value);
+  }
+  
+  const repeatnum = (value: number, times: number): string[] => {
+    return new Array(times).fill(value);
+  }
+  
+  const repeat = <T>(value: T, times: number): string[] => {
+    return new Array(times).fill(value);
+  }
+
+  // const strArry = repeatstr('hello',3);
+  const numArry = repeat<number>(10,3);
+  console.log(numArry);
 };
 
 export default Example;
