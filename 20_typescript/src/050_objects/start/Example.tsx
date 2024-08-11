c9826487ecb4da781659a0f1f4ee491a7455ad63
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-08-11 21:12:04 +0900

    fix typescript interface

--- 20_typescript/src/050_objects/start/Example.tsx
+++ 20_typescript/src/050_objects/start/Example.tsx
@@ -1,5 +1,13 @@
 const Example = () => {
   // 練習用
+  const arry1: number[] = [1,2,3];
+  const arry2: string[] = ['hello','bye'];
+  const arry3: Array<number> = [1,2,3];
+  const arry4: (string | number)[] = [1,'bye'];
+  const arry5: Array<string | number> =  [1,'bye'];
+
+  type Person = { name: string, age?: number}
+  const obj1: Person = {name: 'Taro'};
 };
 
 export default Example;
