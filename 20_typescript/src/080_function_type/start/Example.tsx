90ca03eebb561521240238dd41474fd885c4f00f
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-08-11 21:20:31 +0900

    fix typescript function

--- 20_typescript/src/080_function_type/start/Example.tsx
+++ 20_typescript/src/080_function_type/start/Example.tsx
@@ -1,5 +1,13 @@
 const Example = () => {
   // 練習用
+  function sum1(x:number, y:number) {
+    return x + y;
+  }
+
+  const result = sum1(1,2);
+  console.log(result);
+
+  const sum2 = (x: number, y: number) => x + y;
 };
 
 export default Example;
